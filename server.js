8b1ee8427c139e668b971b9fe671dbc787b8cec1
vikrammvk <mail2me.mvk@gmail.com>
2017-05-21 13:17:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile('resume.html'));
 });
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'sample.html'));
