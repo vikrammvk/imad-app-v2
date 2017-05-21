294d78bb6309ecbc11009a840673ccc6312701ce
vikrammvk <mail2me.mvk@gmail.com>
2017-05-21 13:55:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname,'/','me.jpg'));
+  res.sendFile(path.join(__dirname,'','me.jpg'));
 });
 
 
